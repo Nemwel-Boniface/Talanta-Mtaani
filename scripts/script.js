54a400f56aed53fea8151577d36e3ae7143bf0d7
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-02-02 13:59:23 +0300

    Update to judges information

--- scripts/script.js
+++ scripts/script.js
@@ -27,6 +27,7 @@ function hideMenu() {
 const judgesList = [
   {
     judgeImg: 'images/Nemwel.jpg',
+    alt: 'nemwel',
     chessBoard: 'images/chessboard.png',
     judgeName: 'Nemwel Boniface',
     judgeTitle: 'Director of the Arts centre of music at Nairobi',
@@ -34,24 +35,47 @@ const judgesList = [
   },
   {
     judgeImg: 'images/cecilia.jpg',
+    alt: 'Cecilia',
     chessBoard: 'images/chessboard.png',
     judgeName: 'Cecilia Mwariri',
     judgeTitle: 'Established musical artist',
     judgeDesc: 'Cecilia is an award winning artist with a decade of experience behind her.',
   },
   {
+    id: 'hide',
     judgeImg: 'images/giks.jpg',
+    alt: 'Mike',
     chessBoard: 'images/chessboard.png',
     judgeName: 'Mike Mwangi',
     judgeTitle: 'Ministry of Sports, Culture and Arts',
     judgeDesc: 'Mwangi is the current CS and is the events main host and sponsor.',
   },
   {
+    id: 'hide',
     judgeImg: 'images/Virginia.jpg',
+    alt: 'Sarah',
     chessBoard: 'images/chessboard.png',
     judgeName: 'Sarah Smith',
     judgeTitle: 'Digital content creator',
     judgeDesc: 'Sarah is a reknown digital artist and content creator within East Africa.',
+  },
+  {
+    id: 'hide',
+    judgeImg: 'images/saNene.jpg',
+    alt: 'Sa Nene',
+    chessBoard: 'images/chessboard.png',
+    judgeName: 'Sa Nene',
+    judgeTitle: 'Film producer/ Director at SaNene',
+    judgeDesc: 'Nene is a reknown film producer having some of his films feature at Netflix.',
+  },
+  {
+    id: 'hide',
+    judgeImg: 'images/wanjaG.jpeg',
+    alt: 'Wanja',
+    chessBoard: 'images/chessboard.png',
+    judgeName: 'Wanja Gitau',
+    judgeTitle: 'Actress/ model',
+    judgeDesc: 'Wanja is famous for her roles in Netflix movies such as In From The Cold.',
   },
 ];
 
@@ -62,7 +86,7 @@ function generateSection() {
     judgesSection.innerHTML = `${judgesSection.innerHTML}
     <div class="judge">
           <div class="judgeLeft">
-            <img class="jj" src="${judgesList[i].judgeImg}" alt="Nemwel">
+            <img class="jj" src="${judgesList[i].judgeImg}" alt="${judgesList[i].alt}">
             <img class="bg" src="${judgesList[i].chessBoard}" alt="judge image bg">
           </div>
           <div class="judgeRight">
