f7e680fd9aea8db706d46bd00d725a53ae0d188f
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-02-03 16:48:31 +0300

    Update to form popup scripts

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,22 +1,29 @@
-const popupDetails = [];
+const popupDetails = [
+  {
+    closeF: 'images/formclose.png',
+    logo: 'images/talantamtaanilogo.png',
+    submit: 'Get in touch',
+  }
+];
 
 const body = document.querySelector('body');
 const openForm = document.getElementById('openForm');
 const closeForm = document.getElementById('closeForm');
 
-const popUp = document.createElement('section');
+const popUp = document.createElement('div');
 popUp.classList.add('popup');
 
 function displayForm() {
+  console.log('runs');
   popUp.style.display = 'block';
   body.appendChild(popUp);
   const form = `
   <div class="closeForm">
-      <img id="closeForm" src="formclose.png" alt="Close icon for the png">
+      <img id="closeForm" src="${popupDetails[0].closeF}" alt="Close icon for the png">
     </div>
     <div class="formbody">
       <div class="popuplogo">
-        <img src="images/talantamtaanilogo.png" alt="Talanta Mtaani logo">
+        <img src="${popupDetails[0].logo}" alt="Talanta Mtaani logo">
       </div>
       <form id="form">
         <label for="names">Full name</label><br>
@@ -24,9 +31,21 @@ function displayForm() {
         <label for="email">Email Address</label><br>
         <input type="email" id="email" name="email" placeholder="example@email.com" required><br>
         <textarea maxlength="500" placeholder="Hello I am Nemwel. Have something to tell me?"></textarea><br>
-        <button type="submit">Get in touch</button>
+        <button type="submit">${popupDetails[0].submit}</button>
       </form>
     </div> `;
     popUp.innerHTML = form;
     body.appendChild(popUp);
+
+    console.log('reaches');
+    document.addEventListener('click', (click) => {
+      if (click.target.id === 'closeForm') {
+        popUp.style.display = 'none';
+      }
+    });
 }
+
+openForm.addEventListener('click', () => {
+  console.log('clicked1');
+  displayForm();
+});
