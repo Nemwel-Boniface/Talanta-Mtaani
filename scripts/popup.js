e8404ca93e867978605cb3aed9fd7a5ffad7d887
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-02-03 18:31:55 +0300

    Update to popup scripts

--- scripts/popup.js
+++ scripts/popup.js
@@ -14,30 +14,29 @@ const popUp = document.createElement('div');
 popUp.classList.add('popup');
 
 function displayForm() {
-  console.log('runs');
   popUp.style.display = 'block';
   body.appendChild(popUp);
   const form = `
   <div class="closeForm">
-      <img id="closeForm" src="${popupDetails[0].closeF}" alt="Close icon for the png">
+      <img class="desktopHide" id="closeForm" src="${popupDetails[0].closeF}" alt="Close icon for the png">
+      <img id="X" class="mobileHide" src="${popupDetails[0].closeF}" alt="Close icon for the png">
     </div>
     <div class="formbody">
       <div class="popuplogo">
-        <img src="${popupDetails[0].logo}" alt="Talanta Mtaani logo">
+        <img id="X" src="${popupDetails[0].logo}" alt="Talanta Mtaani logo">
       </div>
-      <form id="form">
+      <form action="https://formspree.io/f/mgedowaj" method="POST" id="form">
         <label for="names">Full name</label><br>
         <input type="text" id="names" name="names" maxlength="30" placeholder="John Doe" required><br>
         <label for="email">Email Address</label><br>
-        <input type="email" id="email" name="email" placeholder="example@email.com" required><br>
-        <textarea maxlength="500" placeholder="Hello I am Nemwel. Have something to tell me?"></textarea><br>
+        <input type="email" id="email" name="_replyto" placeholder="example@email.com" required><br>
+        <textarea maxlength="500" name="message" placeholder="Hello I am Nemwel. Have something to tell me?"></textarea><br>
         <button type="submit">${popupDetails[0].submit}</button>
       </form>
     </div> `;
     popUp.innerHTML = form;
     body.appendChild(popUp);
 
-    console.log('reaches');
     document.addEventListener('click', (click) => {
       if (click.target.id === 'closeForm') {
         popUp.style.display = 'none';
@@ -46,6 +45,11 @@ function displayForm() {
 }
 
 openForm.addEventListener('click', () => {
-  console.log('clicked1');
   displayForm();
+});
+
+document.addEventListener('click', (click) => {
+  if (click.target.id === 'X') {
+    popUp.style.display = 'none';
+  }
 });
